d100930d5edba03b84c8ccc2d0fd8b0e8658a855
KyeongJooni <165972574+KyeongJooni@users.noreply.github.com>
2025-12-09 23:42:05 +0900

    mod: OG태그 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,7 @@ import './globals.css';
 
 export const metadata: Metadata = {
   title: 'KyeongJooni Portfolio',
-  description:
-    '사용자 경험과 구조적 설계에 집중하는 개발자 이경준 포트폴리오',
+  description: '사용자 경험과 구조적 설계에 집중하는 개발자 이경준 포트폴리오',
   keywords: [
     'Frontend Developer',
     'Frontend Engineer',
@@ -23,13 +22,24 @@ export const metadata: Metadata = {
     locale: 'ko_KR',
     url: 'https://www.kyeongjooni.site/',
     title: 'KyeongJooni Portfolio',
-    description: '사용자 경험과 구조적 설계에 집중하는 개발자 이경준 포트폴리오',
+    description:
+      '사용자 경험과 구조적 설계에 집중하는 개발자 이경준 포트폴리오',
     siteName: 'KyeongJooni Portfolio',
+    images: [
+      {
+        url: 'https://www.kyeongjooni.site/portfolio.png',
+        width: 1200,
+        height: 630,
+        alt: 'KyeongJooni Portfolio',
+      },
+    ],
   },
   twitter: {
     card: 'summary_large_image',
     title: 'KyeongJooni Portfolio',
-    description: '사용자 경험과 구조적 설계에 집중하는 개발자 이경준 포트폴리오',
+    description:
+      '사용자 경험과 구조적 설계에 집중하는 개발자 이경준 포트폴리오',
+    images: ['https://www.kyeongjooni.site/portfolio.png'],
   },
   robots: {
     index: true,
